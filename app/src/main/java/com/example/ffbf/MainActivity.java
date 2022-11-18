5e157fd4b9814391d9569c0f300edb4b5d9a39a0
Florin <berdilaflorin@gmail.com>
2022-11-18 20:51:24 +0000

    Remove app bar and make the app fullscreen Design registration screen Create User class

--- app/src/main/java/com/example/ffbf/MainActivity.java
+++ app/src/main/java/com/example/ffbf/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ffbf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Remove toolbar and make the app to run fullscreen
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
     }
+
+
+
+
 }
