e015fe179b5e45c0a9ba79803562edffd93b10a6
Florin <berdilaflorin@gmail.com>
2022-11-27 16:30:02 +0000

    Dashboard and Profile section created Dashboard design and menu finished

--- app/src/main/java/com/example/ffbf/Dashboard.java
+++ app/src/main/java/com/example/ffbf/Dashboard.java
@@ -1,14 +1,41 @@
 package com.example.ffbf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
 
 public class Dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         setContentView(R.layout.activity_dashboard);
+    }
+    //Display the navigation menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_nav, menu);
+        return true;
+    }
+    //Set the paths to every activity inside the navigation
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dashboard:
+                startActivity(new Intent(this, Dashboard.class));
+                break;
+            case R.id.profile:
+                startActivity(new Intent(this, Profile.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
